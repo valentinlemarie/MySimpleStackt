69fba8c7e8cadf8c71f6886527fd465bbe759d62
valentinlemarie <valentin.lemarie@wanadoo.fr>
2015-03-02 10:16:10 +0100

    commit Unversioned files

---
+++ src/main/java/dcll/vlem/mysampletack/App.java
@@ -0,0 +1,15 @@
+package dcll.vlem.mysampletack;
+
+
+public class App{
+    /**
+     *
+     * @param args
+     */
+    public static void main( final String[] args) {
+        /**
+         *
+         */
+        System.out.println("Hello World!");
+    }
+}
